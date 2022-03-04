fb9364b4721724d1ff15543f2abe2697ddee1e17
Ellyzeul <gabrielaug.240@gmail.com>
2022-03-04 12:49:43 -0300

    feat: player camera configured

--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -3,10 +3,12 @@ extends Sprite
 const Player = preload("res://scripts/lib_player.gd")
 var speed
 var velocity
+var camera
 
 func _ready():
 	speed = 5
 	velocity = Vector2()
+	camera = self.get_child(0)
 
 func _process(delta):
 	velocity = Player.get_velocity(speed)
